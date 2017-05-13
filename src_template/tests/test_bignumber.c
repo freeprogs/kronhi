7ebf7be0ab3022cbe64fc72fae53123264820386
Slava <freeprogs.feedback@yandex.ru>
2017-05-13 20:25:34 +1100

    Add C unit-tests infrastructure
    Now, it is possible to make tests from the top of the program.
    One test was added to look that everything works fine.
    
    To add a new test go to `src_template/tests', add the new test and add
    it's build info to the top Makefile. Then run `make' to build sources
    needed to build tests, then run `make tests' to build tests and run
    them. Also you can run tests manually through the runtests.sh script
    (in two modes: silent and verbose) in the `build/tests' directory and
    also you can run tests manually.

---
+++ src_template/tests/test_bignumber.c
@@ -0,0 +1,60 @@
+/*
+ * This file is a part of __PROGRAM_NAME__ __PROGRAM_VERSION__
+ *
+ * __PROGRAM_COPYRIGHT__ __PROGRAM_AUTHOR__ __PROGRAM_AUTHOR_EMAIL__
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <CUnit/Basic.h>
+#include "../bignumber.h"
+
+void test_can_set_value_from_int(void);
+
+int main(void)
+{
+    CU_pSuite suite = NULL;
+
+    if (CU_initialize_registry() != CUE_SUCCESS)
+        return CU_get_error();
+
+    suite = CU_add_suite("suite", NULL, NULL);
+    if (suite == NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (CU_add_test(suite, "can set value from int",
+                    test_can_set_value_from_int) == NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+
+    CU_cleanup_registry();
+    return CU_get_error();
+}
+
+void test_can_set_value_from_int(void)
+{
+    struct bignumber number;
+    char out[1000];
+
+    bignumber_set_value_int(&number, 1);
+    bignumber_tostr(&number, out);
+    CU_ASSERT_STRING_EQUAL(out, "1");
+}
