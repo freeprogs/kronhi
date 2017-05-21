d562311b54bc53a24b795e8901d5f53c11548f8e
Slava <freeprogs.feedback@yandex.ru>
2017-05-22 10:35:39 +1100

    Add a comment to binarycmd_write_dir()

--- src_template/binarycmd.c
+++ src_template/binarycmd.c
@@ -19,6 +19,10 @@
 
 #include "binarycmd.h"
 
+/* binarycmd_write_dir:
+   write directory to the destination file with offset
+   return success code if written without errors
+   return error code if was not written or was written with errors */
 enum binarycmd_code binarycmd_write_dir(
     const char *destination, const struct file_offset *offset,
     const char *dirdesc, enum write_cipher_type cipher)
