96ac270c610d8f4e84604ef8f4be1112bb18c6f4
Slava <freeprogs.feedback@yandex.ru>
2017-05-28 09:17:06 +1100

    Remove old functions from the binddir module

--- src_template/bindir.h
+++ src_template/bindir.h
@@ -20,37 +20,6 @@
 #ifndef BINDIR_H
 #define BINDIR_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "endian.h"
 
-/* maximum length of writable directory header */
-#define BINDIR_MAXHEADER  1 + 2 + 65535 + 4 + 4
-
-/*
- * struct bindir contents:
- * type_sign     --  the sign of the directory header
- * descsize      --  the directory description size
- * descp         --  the directory description itself
- * num_of_files  --  the number of files in the directory
- * file_offset   --  the offset of the first file in the directory
- *                   this offset is relative to its place in memory
-*/
-struct bindir {
-    unsigned char type_sign;
-    unsigned short descsize;
-    unsigned char *descp;
-    unsigned long num_of_files;
-    size_t file_offset;
-};
-
-struct bindir *bindir_create(void);
-int bindir_desc_set(struct bindir *dir, const char *dirdesc);
-void bindir_num_of_files_set(struct bindir *dir, unsigned long num);
-void bindir_file_offset_set(struct bindir *dir, size_t offset);
-void bindir_print(struct bindir *dir);
-void bindir_free(struct bindir *dir);
-size_t bindir_make_bin_header(struct bindir *dir, unsigned char dirheader[]);
 
 #endif
