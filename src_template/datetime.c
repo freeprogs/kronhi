154481947cfb0589b9577115e32d0b0f713ff9cf
Slava <freeprogs.feedback@yandex.ru>
2017-06-08 10:26:21 +1100

    Add the datetime module

---
+++ src_template/datetime.c
@@ -0,0 +1,34 @@
+/*
+ * This file is a part of __PROGRAM_NAME__ __PROGRAM_VERSION__
+ *
+ * __PROGRAM_COPYRIGHT__ __PROGRAM_AUTHOR__ __PROGRAM_AUTHOR_EMAIL__
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "datetime.h"
+
+int datetime_get_now(char out[])
+{
+    time_t t;
+    size_t ret;
+
+    t = time(NULL);
+    ret = strftime(
+        out,
+        DATETIMESIZE,
+        "%Y%m%d%H%M%S",
+        localtime(&t));
+    return ret == DATETIMESIZE - 1;
+}
