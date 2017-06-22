7ccd2872f1fff8dcef73ed44628226e107ad11de
Slava <freeprogs.feedback@yandex.ru>
2017-06-22 17:03:59 +1100

    Add a test for write file in node unit-tests

--- src_template/tests/test_node.c
+++ src_template/tests/test_node.c
@@ -24,6 +24,7 @@
 void test_can_test_for_dir_type(void);
 void test_can_test_for_file_type(void);
 void test_can_write_dir(void);
+void test_can_write_file(void);
 
 int main(void)
 {
@@ -43,7 +44,9 @@ int main(void)
      || CU_add_test(suite, "can write dir",
                     test_can_write_dir) == NULL
      || CU_add_test(suite, "can test for file type",
-                    test_can_test_for_file_type) == NULL) {
+                    test_can_test_for_file_type) == NULL
+     || CU_add_test(suite, "can write file",
+                    test_can_write_file) == NULL) {
         CU_cleanup_registry();
         return CU_get_error();
     }
@@ -139,5 +142,70 @@ void test_can_write_dir(void)
     CU_ASSERT_EQUAL(memcmp(buffer, dirbytes, dirsize), 0);
 
     bindir_end(&dir);
+    fclose(iofp);
+}
+
+void test_can_write_file(void)
+{
+    FILE *iofp;
+    FILE *ifp;
+
+    int i;
+    unsigned char filebytes[100];
+    size_t filesize;
+    struct binfile file;
+    unsigned char buffer[100];
+
+    iofp = tmpfile();
+    if (iofp == NULL)
+        CU_FAIL("can't create temporary file");
+
+    ifp = tmpfile();
+    if (ifp == NULL)
+        CU_FAIL("can't create temporary file");
+
+    for (i = 0; i < 100; i++)
+        putc('\0', iofp);
+    rewind(iofp);
+
+    fprintf(ifp, "abc");
+    rewind(ifp);
+
+    binfile_start(&file);
+
+    filesize = 33;
+    memcpy(filebytes,
+           "\x66"
+           "\x01"
+           "\x61"
+           "\x00\x01"
+           "\x62"
+           "\x32\x30\x31\x37\x30\x31\x30\x32\x30\x33\x30\x34\x30\x35"
+           "\x01\x02\x03\x04"
+           "\x33\x00"
+           "\x61\x62\x63"
+           "\x00\x00\x00\x01",
+           filesize);
+
+    binfile_type_set(&file, 'f');
+    binfile_namesize_set(&file, 1);
+    binfile_name_set(&file, "a");
+    binfile_descsize_set(&file, 1);
+    binfile_desc_set(&file, "b");
+    binfile_datetime_set(&file, "20170102030405");
+    binfile_ctrlsum_set(&file, 0x01020304);
+    binfile_contentsize_set(&file, "3");
+    binfile_contentstream_set(&file, ifp);
+    binfile_file_offset_set(&file, 1);
+
+    node_write_file(iofp, &file);
+    rewind(iofp);
+
+    memset(buffer, 0, sizeof buffer);
+    fread(buffer, 1, sizeof buffer, iofp);
+
+    CU_ASSERT_EQUAL(memcmp(buffer, filebytes, filesize), 0);
+
+    binfile_end(&file);
     fclose(iofp);
 }
