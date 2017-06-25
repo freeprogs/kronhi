96210102b7013117f8741c0ed01b44b3e33405f8
Slava <freeprogs.feedback@yandex.ru>
2017-06-26 09:48:28 +1100

    Add a suite for append file tests in chain unit-tests
    Added tests:
    
    * test_raise_on_absent_directory()

--- src_template/tests/test_chain.c
+++ src_template/tests/test_chain.c
@@ -24,26 +24,40 @@
 void test_raise_on_very_big_file_offset(void);
 void test_raise_on_very_big_directory_size(void);
 void test_can_write_directory_to_the_end_of_file(void);
+void test_raise_on_absent_directory(void);
 
 int main(void)
 {
-    CU_pSuite suite = NULL;
+    CU_pSuite suite1 = NULL;
+    CU_pSuite suite2 = NULL;
 
     if (CU_initialize_registry() != CUE_SUCCESS)
         return CU_get_error();
 
-    suite = CU_add_suite("chain create dir", NULL, NULL);
-    if (suite == NULL) {
+    suite1 = CU_add_suite("chain create dir", NULL, NULL);
+    if (suite1 == NULL) {
         CU_cleanup_registry();
         return CU_get_error();
     }
 
-    if (CU_add_test(suite, "raise on very big file offset",
+    suite2 = CU_add_suite("chain append file", NULL, NULL);
+    if (suite2 == NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (CU_add_test(suite1, "raise on very big file offset",
                     test_raise_on_very_big_file_offset) == NULL ||
-        CU_add_test(suite, "raise on very big directory size",
+        CU_add_test(suite1, "raise on very big directory size",
                     test_raise_on_very_big_directory_size) == NULL ||
-        CU_add_test(suite, "can write directory to the end of file",
+        CU_add_test(suite1, "can write directory to the end of file",
                     test_can_write_directory_to_the_end_of_file) == NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (CU_add_test(suite2, "raise on absent directory",
+                    test_raise_on_absent_directory) == NULL) {
         CU_cleanup_registry();
         return CU_get_error();
     }
@@ -140,5 +154,50 @@ void test_can_write_directory_to_the_end_of_file(void)
 
     CU_ASSERT_EQUAL(retval, CHAIN_OK);
 
+    remove(destination);
+}
+
+void test_raise_on_absent_directory(void)
+{
+    struct chain chain;
+    const char *destination = "file.txt";
+    struct file_offset offset;
+    enum chain_code retval;
+
+    const char *source = "source.txt";
+    const char *filename;
+    const char *filedesc;
+    size_t filereloff;
+
+    FILE *fp;
+    int i;
+
+    fp = fopen(destination, "wb");
+    if (fp == NULL)
+        CU_FAIL("can't create temporary file");
+    for (i = 0; i < 100; i++)
+        putc('x', fp);
+    fclose(fp);
+
+    fp = fopen(source, "wb");
+    if (fp == NULL)
+        CU_FAIL("can't create temporary file");
+    fprintf(fp, "abc");
+    fclose(fp);
+
+    fileoffset_clear(&offset);
+    chain_start(&chain, destination, &offset);
+
+    filename = "a";
+    filedesc = "b";
+    filereloff = 0;
+    retval = chain_append_file(
+        &chain, source, filename, filedesc, filereloff);
+
+    chain_end(&chain);
+
+    CU_ASSERT_EQUAL(retval, CHAIN_ERROR_FILE_NODIR);
+
+    remove(source);
     remove(destination);
 }
