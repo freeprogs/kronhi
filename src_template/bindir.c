3d3b7f5c7df81154eb976cf5913535cd77407ca3
Slava <freeprogs.feedback@yandex.ru>
2017-07-07 15:44:41 +1100

    Add binfield object to the bindir module

--- src_template/bindir.c
+++ src_template/bindir.c
@@ -24,38 +24,43 @@
                  return 0 if an error happened */
 int bindir_start(struct bindir *dir)
 {
-    struct field_raw *rp;
-    struct field_num *np;
+    struct binfield field;
+    struct binfield_raw *rp;
+    struct binfield_num *np;
 
-    rp = binfield_raw_create(_TYPE_SIGN_FIELD_SIZE);
+    binfield_start(&field, NULL);
+
+    rp = binfield_raw_create(&field, _TYPE_SIGN_FIELD_SIZE);
     if (!rp)
         return 0;
     dir->type_sign = rp;
-    binfield_raw_set(rp, "\0", 1);
+    binfield_raw_set(&field, rp, "\0", 1);
 
-    np = binfield_num_create(_DESCSIZE_FIELD_SIZE);
+    np = binfield_num_create(&field, _DESCSIZE_FIELD_SIZE);
     if (!np)
         return 0;
     dir->descsize = np;
-    binfield_num_set(np, "\0", 1);
+    binfield_num_set(&field, np, "\0", 1);
 
-    rp = binfield_raw_create(_DESC_FIELD_SIZE);
+    rp = binfield_raw_create(&field, _DESC_FIELD_SIZE);
     if (!rp)
         return 0;
     dir->desc = rp;
-    binfield_raw_set(rp, "\0", 1);
+    binfield_raw_set(&field, rp, "\0", 1);
 
-    np = binfield_num_create(_NUM_OF_FILES_FIELD_SIZE);
+    np = binfield_num_create(&field, _NUM_OF_FILES_FIELD_SIZE);
     if (!np)
         return 0;
     dir->num_of_files = np;
-    binfield_num_set(np, "\0", 1);
+    binfield_num_set(&field, np, "\0", 1);
 
-    np = binfield_num_create(_FILE_OFFSET_FIELD_SIZE);
+    np = binfield_num_create(&field, _FILE_OFFSET_FIELD_SIZE);
     if (!np)
         return 0;
     dir->file_offset = np;
-    binfield_num_set(np, "\0", 1);
+    binfield_num_set(&field, np, "\0", 1);
+
+    binfield_end(&field);
 
     return 1;
 }
@@ -65,9 +70,17 @@ int bindir_start(struct bindir *dir)
                     return 0 if an error happened */
 int bindir_type_set(struct bindir *dir, char type)
 {
-    return binfield_raw_set(dir->type_sign,
-                            &type,
-                            sizeof type);
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_raw_set(
+        &field,
+        dir->type_sign,
+        &type,
+        sizeof type);
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_type_get: get from directory the type sign field
@@ -75,7 +88,13 @@ int bindir_type_set(struct bindir *dir, char type)
                     return 0 if an error happened */
 int bindir_type_get(const struct bindir *dir, char *out)
 {
-    return binfield_raw_get(dir->type_sign, out);
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_raw_get(&field, dir->type_sign, out);
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_descsize_set: set in directory the description size field
@@ -83,9 +102,17 @@ int bindir_type_get(const struct bindir *dir, char *out)
                         return 0 if an error happened */
 int bindir_descsize_set(struct bindir *dir, unsigned short dirdescsize)
 {
-    return binfield_num_set(dir->descsize,
-                            &dirdescsize,
-                            sizeof dirdescsize);
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_num_set(
+        &field,
+        dir->descsize,
+        &dirdescsize,
+        sizeof dirdescsize);
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_descsize_get: get from directory the description size field
@@ -93,7 +120,13 @@ int bindir_descsize_set(struct bindir *dir, unsigned short dirdescsize)
                             return 0 if an error happened */
 int bindir_descsize_get(const struct bindir *dir, unsigned short *out)
 {
-    return binfield_num_get(dir->descsize, out);
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_num_get(&field, dir->descsize, out);
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_desc_set: set in directory the description field
@@ -101,9 +134,17 @@ int bindir_descsize_get(const struct bindir *dir, unsigned short *out)
                     return 0 if an error happened */
 int bindir_desc_set(struct bindir *dir, const char *dirdesc)
 {
-    return binfield_raw_set(dir->desc,
-                            dirdesc,
-                            strlen(dirdesc));
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_raw_set(
+        &field,
+        dir->desc,
+        dirdesc,
+        strlen(dirdesc));
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_desc_get: get from directory the description field
@@ -111,7 +152,13 @@ int bindir_desc_set(struct bindir *dir, const char *dirdesc)
                     return 0 if an error happened */
 int bindir_desc_get(const struct bindir *dir, char *out)
 {
-    return binfield_raw_get(dir->desc, out);
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_raw_get(&field, dir->desc, out);
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_num_of_files_set: set in directory the number of files field
@@ -119,9 +166,17 @@ int bindir_desc_get(const struct bindir *dir, char *out)
                             return 0 if an error happened */
 int bindir_num_of_files_set(struct bindir *dir, size_t num_of_files)
 {
-    return binfield_num_set(dir->num_of_files,
-                            &num_of_files,
-                            sizeof num_of_files);
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_num_set(
+        &field,
+        dir->num_of_files,
+        &num_of_files,
+        sizeof num_of_files);
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_num_of_files_get: get from directory the number of files field
@@ -129,7 +184,13 @@ int bindir_num_of_files_set(struct bindir *dir, size_t num_of_files)
                             return 0 if an error happened */
 int bindir_num_of_files_get(const struct bindir *dir, size_t *out)
 {
-    return binfield_num_get(dir->num_of_files, out);
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_num_get(&field, dir->num_of_files, out);
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_file_offset_set: set in directory the relative file offset field
@@ -137,9 +198,17 @@ int bindir_num_of_files_get(const struct bindir *dir, size_t *out)
                            return 0 if an error happened */
 int bindir_file_offset_set(struct bindir *dir, size_t file_offset)
 {
-    return binfield_num_set(dir->file_offset,
-                            &file_offset,
-                            sizeof file_offset);
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_num_set(
+        &field,
+        dir->file_offset,
+        &file_offset,
+        sizeof file_offset);
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_file_offset_get: get from directory the relative file offset field
@@ -147,7 +216,13 @@ int bindir_file_offset_set(struct bindir *dir, size_t file_offset)
                            return 0 if an error happened */
 int bindir_file_offset_get(const struct bindir *dir, size_t *out)
 {
-    return binfield_num_get(dir->file_offset, out);
+    struct binfield field;
+    int retval;
+
+    binfield_start(&field, NULL);
+    retval = binfield_num_get(&field, dir->file_offset, out);
+    binfield_end(&field);
+    return retval;
 }
 
 /* bindir_get_size: get directory fields total size
@@ -169,14 +244,20 @@ int bindir_get_size(const struct bindir *dir, size_t *out)
 /* bindir_end: delete directory fields and fill it by zeros */
 void bindir_end(struct bindir *dir)
 {
-    binfield_raw_free(dir->type_sign);
-    binfield_num_free(dir->descsize);
-    binfield_raw_free(dir->desc);
-    binfield_num_free(dir->num_of_files);
-    binfield_num_free(dir->file_offset);
+    struct binfield field;
+
+    binfield_start(&field, NULL);
+
+    binfield_raw_free(&field, dir->type_sign);
+    binfield_num_free(&field, dir->descsize);
+    binfield_raw_free(&field, dir->desc);
+    binfield_num_free(&field, dir->num_of_files);
+    binfield_num_free(&field, dir->file_offset);
     dir->type_sign = NULL;
     dir->descsize = NULL;
     dir->desc = NULL;
     dir->num_of_files = NULL;
     dir->file_offset = NULL;
+
+    binfield_end(&field);
 }
