2f6ea03bdba95c7ccbcfc75d42fd9b188b98f34a
Slava <freeprogs.feedback@yandex.ru>
2017-07-19 22:33:56 +1100

    Add a test for write raw field in binfield unit-tests

--- src_template/tests/test_binfield.c
+++ src_template/tests/test_binfield.c
@@ -26,6 +26,7 @@ void test_can_set_raw_field(void);
 void test_raise_on_set_raw_field_with_value_overflow(void);
 void test_can_get_raw_field(void);
 void test_can_read_raw_field(void);
+void test_can_write_raw_field(void);
 
 int main(void)
 {
@@ -49,7 +50,9 @@ int main(void)
      || CU_add_test(suite1, "test can get raw field",
                     test_can_get_raw_field) == NULL
      || CU_add_test(suite1, "test can read raw field",
-                    test_can_read_raw_field) == NULL) {
+                    test_can_read_raw_field) == NULL
+     || CU_add_test(suite1, "test can write raw field",
+                    test_can_write_raw_field) == NULL) {
         CU_cleanup_registry();
         return CU_get_error();
     }
@@ -195,6 +198,46 @@ void test_can_read_raw_field(void)
     CU_ASSERT_EQUAL(retval, 1);
     CU_ASSERT_NSTRING_EQUAL(data->val, "abc", vlen);
     CU_ASSERT_EQUAL(data->len, vlen);
+
+    binfield_end(&field);
+
+    fclose(iofp);
+}
+
+void test_can_write_raw_field(void)
+{
+    struct binfield field;
+    struct binfield_raw *data;
+    size_t maxsize = 3;
+
+    FILE *iofp;
+    unsigned char value[100];
+    size_t vlen;
+    int retval;
+
+    iofp = tmpfile();
+    if (iofp == NULL)
+        CU_FAIL("can't create temporary file");
+
+    binfield_start(&field, NULL);
+
+    data = binfield_raw_create(&field, maxsize);
+
+    CU_ASSERT_PTR_NOT_NULL(data);
+
+    vlen = 3;
+    memcpy(data->val, "abc", vlen);
+    data->len = vlen;
+
+    retval = binfield_raw_write(&field, data, iofp);
+
+    CU_ASSERT_EQUAL(retval, 1);
+
+    rewind(iofp);
+    memset(value, 0, vlen);
+    CU_ASSERT_EQUAL(fread(value, 1, vlen, iofp), vlen);
+
+    CU_ASSERT_NSTRING_EQUAL(value, data->val, vlen);
 
     binfield_end(&field);
 
